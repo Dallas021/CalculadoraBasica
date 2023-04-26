62338a98a7e7b9f33479ec59aa92624be0af8ba5
Dallas021 <126030227+Dallas021@users.noreply.github.com>
2023-04-26 19:05:20 -0300

    Add files via upload

---
+++ Calculadora/index.js
@@ -0,0 +1,9 @@
+const div = require("./div/div")
+const mult = require("./mult/mult")
+const soma = require("./soma/soma")
+const sub = require("./sub/sub") 
+
+console.log(div(4,2))
+console.log(mult(4,2))
+console.log(soma(4,2))
+console.log(sub(4,2))
